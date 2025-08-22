00e22328c20866a17486088187df1b08e070ba61
pcaceresva <pcaceresva@gmail.com>
2025-08-21 22:37:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
-# app.py
 from fastapi import FastAPI
 from pybit.unified_trading import HTTP
 
 app = FastAPI()
 
-# Inicializa sesión Demo de Bybit Unified Trading
 session = HTTP(
     demo=True,
-    api_key="kAEstgmtlzcLtBUC9D",
-    api_secret="Qzn86OWLpLfLdHrGNOq8V6Vcli6oRiP0XJhG",
+    api_key="TU_API_KEY",
+    api_secret="TU_API_SECRET",
 )
 
 @app.get("/test-balance")
 def test_balance():
     try:
-        balance = session.get_wallet_balance(category="spot")
+        balance = session.get_wallet_balance(accountType="UNIFIED")
         return balance
     except Exception as e:
         return {"error": str(e)}
